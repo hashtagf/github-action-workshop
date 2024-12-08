92629c38666fbf15beebb2135bff71fc0444c881
hashtagf <flamezaxaou1@gmail.com>
2024-12-08 21:52:51 +0700

    update: modify automate.bash to accept Docker Hub credentials as parameters and update workflow.yaml to use checkout action v4

--- .bash/automate.bash
+++ .bash/automate.bash
@@ -3,13 +3,16 @@
 # cargo check
 # cargo test
 
+DOCKER_HUB_USERNAME=$1
+DOCKER_HUB_PASSWORD=$2
+
 export TAG=$(cat Cargo.toml | grep version | head -n 1 | awk -F '"' '{print $2}')
 echo "TAG: $TAG"
 
-docker build . -t flhpktech/rust-demo:$TAG
+# docker build . -t flhpktech/rust-demo:$TAG
 
 
-echo $PASSWORD_DOCKER | docker login -u flhpktech --password-stdin
+echo $DOCKER_HUB_PASSWORD | docker login -u $DOCKER_HUB_USERNAME --password-stdin
 docker push flhpktech/rust-demo:$TAG
 
 
